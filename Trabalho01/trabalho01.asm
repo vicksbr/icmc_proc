8ac61a593af754b77c92702bf00c1f96a0530b40
vicks <pedropuzzi@gmail.com>
2014-04-13 22:58:04 -0300

    identação linda

--- Trabalho01/trabalho01.asm
+++ Trabalho01/trabalho01.asm
@@ -23,16 +23,16 @@ vetorSaida: var #16
 vetorAuxiliar: var #16
 
 tam: var #1
-static tam + #0, #16   		; tamanho de vetor
+static tam + #0, #16   		    ;	tamanho de vetor
 
 main:
 
-	loadn R0,#tam 				;r0 recebe a posição da variavel tamanho
-	loadi R1,R0 				;r1 recebe o valor de r0, no caso de 16
-	loadn R0,#vetorEntrada      ;r0 recebe a posição do vetor a ser ordenado
+	loadn R0,#tam 				;	r0 recebe a posição da variavel tamanho
+	loadi R1,R0 				;	r1 recebe o valor de r0, no caso de 16
+	loadn R0,#vetorEntrada      ;	r0 recebe a posição do vetor a ser ordenado
 
 
-	;imprime o vetor original
+							    ;	imprime o vetor original
 
 	mov R2, R1
 	loadn R1,#0
@@ -42,14 +42,6 @@ main:
 	loadn R2,#vetorSaida
 	call copiaVetor
 
-	;mov R0, R2
-	;loadn R1, #240
-	;loadn R2, #16
-	;call printVetor
-
-	;push R0
-	;push R1
-	;push R2
 
 	mov R0, R2
 
@@ -59,10 +51,6 @@ main:
 	loadn R1, #80
 	loadn R2, #16
 	call printVetor
-
-	;pop R2
-	;pop R1
-	;pop R0
 
 halt
 
@@ -75,7 +63,7 @@ halt
 ;----------------------------------------------------
 ;	copiaVetor(R0,R1,R2) -> R2
 ; 	copia um vetor de um registrador para o outro
-
+;
 ;	entradas:
 ;	R0 ponteiro para o vetor original
 ;   R1 tamanho do vetor original
@@ -99,7 +87,7 @@ copiaVetor:
 		inc R0
 		inc R2
 		dec R1
-		jnz copiaVetor_loop
+		jnz copiaVetor_loop 
 
 	pop R3
 	pop R2
@@ -112,7 +100,7 @@ rts
 ;----------------------------------------------------
 ;	printNumero(R0,R1)
 ; 	printa um numero inteiro r0 comecando pel posicao r1 na tela
-
+;
 ;	entradas:
 ;	R0 numero inteiro entre -65534 e 65535 (inteiro 16 bits com sinal)
 ;   R1 posicao da tela onde sera impresso
@@ -130,18 +118,18 @@ printNumero:
 	push R4
 	push R5
 
-	loadn R3, #10    				; atribuindo base 10 para a multiplicação
-	loadn R4, #48    				; variavel auxiliar a ser somada para se obter o numero ASCII
-	loadn R5, #0     				; numero de casas decimais empilhadas
+	loadn R3, #10    				;	atribuindo base 10 para a multiplicação
+	loadn R4, #48    				;	variavel auxiliar a ser somada para se obter o numero ASCII
+	loadn R5, #0     				;	numero de casas decimais empilhadas
 
 	printNumero_empilha:
 
-		mod R2, R0, R3   				; Pega o digito mais significativo
-		add R2, R2, R4   				; Soma 48 para obter o numero ASCII correto
+		mod R2, R0, R3   		    ;	Pega o digito mais significativo
+		add R2, R2, R4   			;	Soma 48 para obter o numero ASCII correto
 		push R2
 		inc R5
-		div R0, R0, R3   				; Divide por 10 para pegar o proximo digito
-		jnz printNumero_empilha 	; Continua empilhando até o resultado da divisao ser 0
+		div R0, R0, R3   			;	Divide por 10 para pegar o proximo digito
+		jnz printNumero_empilha 	;	Continua empilhando até o resultado da divisao ser 0
 
 
 	printNumero_desempilha:
@@ -165,7 +153,7 @@ rts
 ;----------------------------------------------------
 ;	printVetor(R0,R1,R2)
 ; 	printa um vetor R0 de tamanho R2 na posicao R1 da tela
-
+;
 ;	entradas:
 ;	R0 posicao inicial do vetor
 ;   R1 posicao da tela onde sera impresso
@@ -183,13 +171,13 @@ printVetor:
 	push R2
 	push R3
 
-	mov R3, R0    		   ; Carrega a posicao inicial do vetor para ser usada na funcao de printNumero
+	mov R3, R0    		   ;	Carrega a posicao inicial do vetor para ser usada na funcao de printNumero
 
 	printVetor_loop:
 
-		loadi R0, R3 	   ; Carregando o numero do vetor em R0
+		loadi R0, R3 	   ;	Carregando o numero do vetor em R0
 		call printNumero
-		inc R1             ; Percorrendo o vetor e decrementando o tamanho para o fim do loop
+		inc R1             ;	Percorrendo o vetor e decrementando o tamanho para o fim do loop
 		inc R3
 		dec R2
 		jnz printVetor_loop
@@ -220,18 +208,18 @@ printString:
 	push R2
 	push R3
 
-	loadn R3, #0     		    ; Carrega para ser comparado com '\0'
+	loadn R3, #0     		    ;	Carrega para ser comparado com '\0'
 
 	printString_loop:
 
-		loadi R2,R0  		    ; Carrega em r2 o conteudo de mem[R0]
-		cmp R2,R3    		    ; Compara R2 com R3 a procura do fim da string
+		loadi R2,R0  		    ;	Carrega em r2 o conteudo de mem[R0]
+		cmp R2,R3    		    ;	Compara R2 com R3 a procura do fim da string
 		jeq String_loop_fim
 
-		;Se nao achou '/0' imprime o caracter
+							    ;	Se nao achou '/0' imprime o caracter
 
-		outchar R2,R1          ; Imprime o caracter e incrementa a posicao
-		inc R0                 ; para continumar o loop
+		outchar R2,R1           ;	Imprime o caracter e incrementa a posicao
+		inc R0                  ;	para continumar o loop
 		inc R1
 		jmp printString_loop
 
@@ -247,7 +235,7 @@ rts
 ;----------------------------------------------------
 ;	bubble(R0,R1) -> R0
 ; 	printa um vetor R0 de tamanho R2 na posicao R1 da tela
-
+;
 ;	entradas:
 ;	R0 posicao inicial do vetor a ser ordenado
 ;   R1 tamanho do vetor
@@ -258,27 +246,27 @@ rts
 
 bubble:
 
-	push r0  ; vetor entrada
-	push r1  ; tamanho
-	push r2  ; indice i
-	push r3  ; indice j
-	push r4  ; vetor entrada [j-1]
-	push r5  ; vetor entrada [j]
+	push r0  		;	vetor entrada
+	push r1  		;	tamanho
+	push r2  		;	indice i
+	push r3  		;	indice j
+	push r4  		;	vetor entrada [j-1]
+	push r5  		;	vetor entrada [j]
 
-	mov r2,r0      ;r2 recebe a posição do inicio do vetor (ponteiro para o vetor)
-	add r1,r0,r1   ;r1 passa a armazenar a posição na memória do fim do vetor (posição do último elemento)
+	mov r2,r0       ;	r2 recebe a posição do inicio do vetor (ponteiro para o vetor)
+	add r1,r0,r1    ;	r1 passa a armazenar a posição na memória do fim do vetor (posição do último elemento)
 
     dec R1
+    				;	primeiro for (for(i=0;i<n;i++))
+	
+	bubble_loop_1:	
+		mov r3,r0                   ;	r3 recebe a posição do inicio do vetor (ponteiro para o vetor)
 
-	bubble_loop_1:	;primeiro for (for(i=0;i<n;i++))
+		bubble_loop_2:	            ;	segundo for (for(j=0;j<n-1;j++))
 
-		mov r3,r0;	r3 recebe a posição do inicio do vetor (ponteiro para o vetor)
-
-		bubble_loop_2:	;segundo for (for(j=0;j<n-1;j++))
-
-			loadi r4,r3;	r4 <- vet[j]
+			loadi r4,r3             ;	r4 <- vet[j]
 			inc r3
-			loadi r5,r3;	r5 <- vet[j+1]
+			loadi r5,r3             ;	r5 <- vet[j+1]
 
 			cmp r4,r5				;   comparando vet[j] com vet[j+1]
 			jel bubble_dont_swap	;   se i < j, não trocar
@@ -331,29 +319,29 @@ merge:
 	push R7
 
 
-	loadn R2,#4 	; Carregando para R2 o valor minimo do vetor para iniciar o bubble sort
+	loadn R2,#4 	   ;	Carregando para R2 o valor minimo do vetor para iniciar o bubble sort
 	cmp R1,R2
 	cel bubble
 	jel mergeFim
 
-	;Caso o tamanho do vetor ainda seja maior que 4, continua o merge
+					   ;	Caso o tamanho do vetor ainda seja maior que 4, continua o merge
 
-	mov R3,R1          ; Carrega em R3 o tamanho do vetor contido em R1
-	shiftr0 R1, #1     ; Dividindo R1 por 2 e pegando a parte mais significativa
+	mov R3,R1          ;	Carrega em R3 o tamanho do vetor contido em R1
+	shiftr0 R1,#1      ;	Dividindo R1 por 2 e pegando a parte mais significativa
 
 
-	call merge 		   ; Chama o merge para a primeira metade
+	call merge 		   ;	Chama o merge para a primeira metade
 
 	push R0
 	push R1
 
-	add R0, R0, R1    ; R0 = R0 + R1 é a posicao da matade ae o fim do vetor
-	sub R1, R3, R1    ; é o novo tamanho do vetor
+	add R0, R0, R1     ;	R0 = R0 + R1 é a posicao da matade ae o fim do vetor
+	sub R1, R3, R1     ;	é o novo tamanho do vetor
 
-	call merge        ; Chama o merge para a segunda metade
+	call merge         ;	Chama o merge para a segunda metade
 
-	mov R2, R0        ; R2 recebe a posicao para o começo do vetor
-	mov R3, R1        ; R1 recebe o tamanho
+	mov R2, R0         ;	R2 recebe a posicao para o começo do vetor
+	mov R3, R1         ;	R1 recebe o tamanho
 
 	pop R1
 	pop R0
@@ -405,13 +393,13 @@ mergeSort:
 	push R7
 
 
-	add R1, R1, R0     ; R1 = R1 + R0 armazena a última posição do primeiro vetor
-	add R3, R3, R2     ; R3 = R3 + R2 armazena a última posição do segundo vetor
+	add R1, R1, R0     ;	R1 = R1 + R0 armazena a última posição do primeiro vetor
+	add R3, R3, R2     ; 	R3 = R3 + R2 armazena a última posição do segundo vetor
 
 	loadn R4,#vetorAuxiliar
 
-	loadi R5,R0       ; R5 = vetor1[R0]
-	loadi R6,R2       ; R6 = vetor2[R2]
+	loadi R5,R0        ;	R5 = vetor1[R0]
+	loadi R6,R2        ;	R6 = vetor2[R2]
 
 	mergeSort_loop:
 
@@ -488,9 +476,3 @@ mergeSort:
 		pop R0
 
 rts
-
-
-
-
-
-
